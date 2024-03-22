b4ab6631647799bf86dde22aacfcca764803d778
joshreiss <joshua.reiss@qmul.ac.uk>
2024-03-22 09:42:43 +0000

    phase vocoder updates

--- 07 Overdrive Distortion Fuzz/Source code - Distortion-new/Source/PluginProcessor.cpp
+++ 07 Overdrive Distortion Fuzz/Source code - Distortion-new/Source/PluginProcessor.cpp
@@ -146,62 +146,57 @@ void DistortionAudioProcessor::processBlock (juce::AudioBuffer<float>& buffer, j
     for (auto i = numOutputChannels; i < numInputChannels; ++i)
         buffer.clear (i, 0, buffer.getNumSamples());
 
-    for (int i = 0; i < buffer.getNumSamples(); ++i)
+    for (int i = 0; i < buffer.getNumSamples(); ++i) 
     {
-      float inputSignal = 0.44 * sinf(juce::MathConstants<float>::twoPi * inputPhase) + 0.38 * sinf(juce::MathConstants<float>::twoPi * inputPhase) + 0.3f;
-      auto sample = gain * inputSignal;
-      switch (distortionType)
-      {
-        case 0://kHardClipping:
+        for (int j = 0; j < numInputChannels; ++j) 
         {
-          const float threshold = 0.5f;
-          if (sample > threshold) sample = threshold;         // positive clip
-          else if (sample < -threshold) sample = -threshold;  // negative clip
-          break;
-        }
-        case 1://kSoftClipping:
-        {
-          const float threshold1 = 1.0f / 3.0f;
-          const float threshold2 = 2.0f / 3.0f;
-          if (sample > threshold1)
+          auto sample = gain * buffer.getReadPointer(j)[i];
+          switch (distortionType)
           {
-            if (sample > threshold2) sample = 0.5f;         // positive clip
-            else sample = (3.0f - (2.0f - 3.0f * sample) * (2.0f - 3.0f * sample)) / 6.0f;// soft knee (positive)
+            case 0://kHardClipping:
+            {
+              const float threshold = 0.5f;
+              if (sample > threshold) sample = threshold;         // positive clip
+              else if (sample < -threshold) sample = -threshold;  // negative clip
+              break;
+            }
+            case 1://kSoftClipping:
+            {
+              const float threshold1 = 1.0f / 3.0f;
+              const float threshold2 = 2.0f / 3.0f;
+              if (sample > threshold1)
+              {
+                if (sample > threshold2) sample = 0.5f;         // positive clip
+                else sample = (3.0f - (2.0f - 3.0f * sample) * (2.0f - 3.0f * sample)) / 6.0f;// soft knee (positive)
+              }
+              else if (sample < -threshold1)
+              {
+                if (sample < -threshold2) sample = -0.5f;       // negative clip
+                else sample = -(3.0f - (2.0f + 3.0f * sample) * (2.0f + 3.0f * sample)) / 6.0f;// soft knee (negative)
+              }
+              break;
+            }
+            case 2: //SoftClippingExp:
+            {
+              if (sample > 0.0f) sample = 1.0f - expf(-sample);// positive
+              else sample = -1.0f + expf(sample);// negative
+              break;
+            }
+            case 3: //kFullWaveRectifier:
+            {
+              sample = fabs(sample);
+              break;
+            }
+            case 4: //kHalfWaveRectifier:
+            {
+              sample = 0.5f * (fabs(sample) + sample);
+              break;
+            }
+            default:
+              break;
           }
-          else if (sample < -threshold1)
-          {
-            if (sample < -threshold2) sample = -0.5f;       // negative clip
-            else sample = -(3.0f - (2.0f + 3.0f * sample) * (2.0f + 3.0f * sample)) / 6.0f;// soft knee (negative)
-          }
-          break;
+          buffer.getWritePointer(j)[i] = sample;
         }
-        case 2: //SoftClippingExp:
-        {
-          if (sample > 0.0f) sample = 1.0f - expf(-sample);// positive
-          else sample = -1.0f + expf(sample);// negative
-          break;
-        }
-        case 3: //kFullWaveRectifier:
-        {
-          sample = fabs(sample);
-          break;
-        }
-        case 4: //kHalfWaveRectifier:
-        {
-          sample = 0.5f * (fabs(sample) + sample);
-          break;
-        }
-        default:
-          break;
-      }
-
-      for (int j = 0; j < numInputChannels; ++j) buffer.getWritePointer(j)[i] = sample;
-      //for (int j = 0; j < numInputChannels; ++j) buffer.getWritePointer(j)[i] = buffer.getReadPointer(j)[i] * (1.0f - depth * amount);
-
-      // Update input phase
-      inputPhase += 1000 / sampleRate;
-      while (inputPhase >= 1.0) inputPhase -= 1.0;
-
     }
 }
 
